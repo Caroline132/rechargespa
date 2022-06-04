51f654515d92c3cd5f2f20d3bdef067cd9d05782
Caroline Tippins <carotippins@hotmail.com>
2022-06-04 14:10:49 -0400

    Created separate js file for confirmation page

---
+++ confirmation.js
@@ -0,0 +1,17 @@
+var name = sessionStorage.getItem("name");
+document.getElementById('name').innerText = "Hello, "+name;
+
+var email = sessionStorage.getItem("email");
+document.getElementById('emailEntered').innerText = email;
+
+var date = sessionStorage.getItem("date");
+document.getElementById('startDateSelected').innerText = date;
+
+var time = sessionStorage.getItem("time");
+document.getElementById('timeSelected').innerText = "at "+time;
+
+var service = sessionStorage.getItem("service");
+document.getElementById('service').innerText = service;
+
+var expert = sessionStorage.getItem("expert");
+document.getElementById('expert').innerText = "with "+expert;
